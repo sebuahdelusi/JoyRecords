c21f9716e9d52d9f7c1088da8aa5fd8e41ac9b15
@joyastaga <joy@joyastagas-MacBook-Pro.local>
2026-05-11 20:32:42 +0700

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ app.use(cors());
 const app  = express();
 const PORT = process.env.PORT || 8080;
 
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 // ── Middleware ──────────────────────────────────────────────────────────────
 app.use(cors({ origin: '*' }));
 app.use(express.json());
--- frontend/app.js
+++ frontend/app.js
@@ -9,7 +9,6 @@
 // const API_BASE = 'http://localhost:3000/api/v1/jurnal';
 // const API_BASE = 'http://34.50.75.107:3000/api/v1/jurnal';
 
-// Ganti dengan URL Cloud Run backend kamu
 const API_BASE = 'https://joyrecords-backend-123230230-487072029768.asia-southeast2.run.app/api/v1/jurnal';
 
 // ── State ───────────────────────────────────────────────────────
